6b34edfc22ef8ead9b874c7cf8f4ef8668aaaba6
donPabloHDF <donpabloeldefelicitas@gmail.com>
2022-11-15 12:45:14 +0100

    Limpio

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
@@ -14,12 +13,11 @@ import { Footer } from "./component/footer";
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+    const basename = process.env.BASENAME || "BASENAME=/";
 
     return (
-        <div>
+        <>
             <BrowserRouter basename={basename}>
-                <ScrollToTop>
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
@@ -28,9 +26,8 @@ const Layout = () => {
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
-                </ScrollToTop>
             </BrowserRouter>
-        </div>
+        </>
     );
 };
 
